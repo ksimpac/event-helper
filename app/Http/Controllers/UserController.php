2f18fc7499c67cf09d4a1bb5301128fd568fe0d8
ksimpac <adasl0376210456789@gmail.com>
2020-09-17 10:25:37 +0800

    adjust users.show blade

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,11 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\User;
-use Barryvdh\Debugbar\Facade as DebugBar;
 use Illuminate\Support\Facades\DB;
 //use Barryvdh\Debugbar\Facade as Debugbar;
+use Carbon\Carbon;
 
 class UserController extends Controller
 {
@@ -16,37 +15,73 @@ class UserController extends Controller
     }
 
     public function update(User $user)
-    {  
-        
-        if(request()->input('telephone') !== $user->telephone) {
+    {
+
+        if (request()->input('telephone') !== $user->telephone) {
             $data = request()->validate([
-                'telephone' =>['required', 'size:10', 'unique:users']
+                'telephone' => ['required', 'size:10', 'unique:users']
             ]);
             $user->update($data);
         }
-        
-        if(request()->input('email') !== $user->email) {
+
+        if (request()->input('email') !== $user->email) {
             $data = request()->validate([
                 'email' => ['required', 'string', 'email', 'max:255', 'unique:users']
             ]);
             $user->update($data);
             //$user->sendEmailVerificationNotification(); //重送驗證信件
         }
-        
+
 
         return redirect()->back();
     }
 
     public function show(User $user)
-    { 
+    {
         $participants = DB::table('events')
-                        ->join('participants', 'events.event_id', '=', 'participants.event_id')
-                        ->where('user_id', $user->user_id)->get();
-    
-        $collections = DB::table('events')
-                        ->join('collections', 'events.event_id', '=', 'collections.event_id')
-                        ->where('user_id', $user->user_id)->get();
+            ->join('participants', 'events.event_id', '=', 'participants.event_id')
+            ->where('user_id', $user->user_id)->get();
 
-        return view('users.show', compact('participants','collections'));
+        foreach ($participants as $participant) {
+            $participant->dateStart = $this->dateTimeFormat($participant->dateStart, "Add Week");
+            $participant->dateEnd = $this->dateTimeFormat($participant->dateEnd, "Add Week");
+        }
+
+        $collections = DB::table('events')
+            ->join('collections', 'events.event_id', '=', 'collections.event_id')
+            ->where('user_id', $user->user_id)->get();
+
+        foreach ($collections as $collection) {
+            $collection->dateStart = $this->dateTimeFormat($collection->dateStart, "Add Week");
+            $collection->dateEnd = $this->dateTimeFormat($collection->dateEnd, "Add Week");
+        }
+
+        return view('users.show', compact('participants', 'collections'));
+    }
+
+    private function dateTimeFormat($dateString, $option)
+    {
+        if ($option == "Add Week") {
+            $weekMap = [
+                0 => '日',
+                1 => '一',
+                2 => '二',
+                3 => '三',
+                4 => '四',
+                5 => '五',
+                6 => '六',
+            ];
+
+            $dayOfTheWeek = Carbon::parse($dateString)->dayOfWeek;
+            $date = explode(" ", $dateString);
+            return $date[0] . "(" . $weekMap[$dayOfTheWeek] . ") " . substr($date[1], 0, 5);
+        }
+
+        if ($option == "Google Calendar") {
+            /**
+             * 因為Google Calendar會自動將輸入時間格式轉成使用者所在時區的時間，故要先減掉8小時
+             */
+            return Carbon::parse($dateString)->format("Ymd") . "T" . Carbon::parse($dateString)->subHours(8)->format("His") . "Z";
+        }
     }
 }
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -13,10 +13,14 @@
         <div class="col-sm-4">
           <div class="card">
             <img class="card-img-top" src="{{ asset('/storage/image/index').'/'.$participant->imageName }}" alt="Card image cap">
-            <div class="card-body">
+            <div class="card-body text-center">
               <h5 class="card-title">{{ $participant->title }}</h5>
               <p class="card-text">{{ $participant->title }}</p>
-              <p class="card-text">{{ substr($participant->dateStart, 0, 16)." ~ ".substr($participant->dateEnd, 0, 16) }}</p>
+              <p class="card-text">
+                <i class="far fa-clock fa-1x"></i>{{ $participant->dateStart }}<br />
+                至<br />
+                <i class="far fa-clock fa-1x"></i>{{ $participant->dateEnd }}<br />
+              </p>
             </div>
             <div class="card-footer d-flex">
               <a href="/events/{{ $participant->event_id }}" class="btn btn-primary mr-auto p-2">查看詳情</a>
@@ -32,7 +36,7 @@
     @empty
         <h3>目前尚無參與活動</h3>
     @endforelse
-    
+
     <h1 class="mt-3">我的收藏</h1>
     @forelse ($collections as $collection)
         @if($loop->index % 3 == 0)
@@ -44,10 +48,14 @@
         <div class="col-sm-4">
           <div class="card">
             <img class="card-img-top" src="{{ asset('/storage/image/index').'/'.$collection->imageName }}" alt="Card image cap">
-            <div class="card-body">
+            <div class="card-body text-center">
               <h5 class="card-title">{{ $collection->title }}</h5>
               <p class="card-text">{{ $collection->title }}</p>
-              <p class="card-text">{{ substr($collection->dateStart, 0, 16)." ~ ".substr($collection->dateEnd, 0, 16) }}</p>
+              <p class="card-text">
+                <i class="far fa-clock fa-1x"></i>{{ $collection->dateStart }}<br />
+                至<br />
+                <i class="far fa-clock fa-1x"></i>{{ $collection->dateEnd }}<br />
+              </p>
             </div>
             <div class="card-footer d-flex">
               <a href="/events/{{ $collection->event_id }}" class="btn btn-primary mr-auto p-2">查看詳情</a>
