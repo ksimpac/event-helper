bf37d51ccd2463276b3880473ef0a34b50c005b8
ksimpac <adasl0376210456789@gmail.com>
2020-10-21 17:17:06 +0800

    Increase Indent

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -14,9 +14,9 @@
     @include('events.carousel')
 
     <div class="container">
-    <div class="row justify-content-center">
-        <h1>{{ $param }}活動</h1>
-    </div>
-    @include('events.card')
+        <div class="row justify-content-center">
+            <h1>{{ $param }}活動</h1>
+        </div>
+        @include('events.card')
     </div>
 @endsection
