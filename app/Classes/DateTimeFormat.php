8c6fda7fff5aab584ada9888bb5a17bdfb3eb2f0
ksimpac <adasl0376210456789@gmail.com>
2021-04-07 16:30:03 +0800

    create DateTimeFormat class

---
+++ app/Classes/DateTimeFormat.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Classes;
+
+use Carbon\Carbon;
+
+class DateTimeFormat
+{
+    static public function parse($dateString, $option) //時間格式
+    {
+        if ($option == "Add Week") {
+            $weekMap = [
+                0 => '日',
+                1 => '一',
+                2 => '二',
+                3 => '三',
+                4 => '四',
+                5 => '五',
+                6 => '六',
+            ];
+
+            $dayOfTheWeek = Carbon::parse($dateString)->dayOfWeek;
+            $date = explode(" ", $dateString);
+            return $date[0] . "(" . $weekMap[$dayOfTheWeek] . ") " . substr($date[1], 0, 5);
+        }
+
+        if ($option == "Google Calendar") {
+            /**
+             * 因為Google Calendar會自動將輸入時間格式轉成使用者所在時區的時間，故要先減掉8小時
+             */
+            return Carbon::parse($dateString)->format("Ymd") . "T" . Carbon::parse($dateString)->subHours(8)->format("His") . "Z";
+        }
+    }
+}
