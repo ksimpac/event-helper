2e5eac5b63927cf68e8e5b72b3f1a3492d6ec339
ksimpac <adasl0376210456789@gmail.com>
2021-04-12 14:39:03 +0800

    adjust format and fix bug

--- resources/views/events/carousel.blade.php
+++ resources/views/events/carousel.blade.php
@@ -1,35 +1,39 @@
 <div class="container">
-    <div class="row justify-content-center">
-        <div id="carousel" class="carousel slide mb-4 w-100" data-ride="carousel">
-            <ol class="carousel-indicators">
-                @for ($i = 0; $i < $carouselsCount; $i++) <li data-target="#carousel" data-slide-to="{{ $i }}"
-                    @if($i==0) class="active" @endif>
-                    </li>
-                    @endfor
-            </ol>
-            <div class="carousel-inner">
-                @forelse ($carousels as $carousel)
-                <div class="carousel-item @if($loop->index == 0) active @endif">
-                    <img src="{{ asset(Storage::url($carousel->path)) }}" class="d-block w-100">
-                </div>
-                @empty
-                <div class="carousel-item active">
-                    <img src="http://fakeimg.pl/1999x664/" class="d-block w-100">
-                </div>
-                <div class="carousel-item">
-                    <img src="http://fakeimg.pl/1999x664/" class="d-block w-100">
-                </div>
-                @endforelse
-            </div>
+  <div class="row justify-content-center">
+    <div id="carousel" class="carousel slide mb-4 w-100" data-ride="carousel">
+      <ol class="carousel-indicators">
+        @for ($i = 0; $i < $carouselsCount; $i++)
+            <li data-target="#carousel" data-slide-to="{{ $i }}" @if($i == 0) class="active" @endif></li>
+        @endfor
 
-            <a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#carousel" role="button" data-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-            </a>
-        </div>
+        @if($carouselsCount == 0)
+            <li data-target="#carousel" data-slide-to="0" class="active"></li>
+            <li data-target="#carousel" data-slide-to="1"></li>
+        @endif
+      </ol>
+      <div class="carousel-inner">
+        @forelse ($carousels as $carousel)
+            <div class="carousel-item @if($loop->index == 0) active @endif">
+                <img src="{{ asset(Storage::url($carousel->path)) }}" class="d-block w-100">
+            </div>
+        @empty
+            <div class="carousel-item active">
+                <img src="http://fakeimg.pl/1999x664/" class="d-block w-100">
+            </div>
+            <div class="carousel-item">
+                <img src="http://fakeimg.pl/1999x664/" class="d-block w-100">
+            </div>
+        @endforelse
+      </div>
+
+      <a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carousel" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
     </div>
+  </div>
 </div>
