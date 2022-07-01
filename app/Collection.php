db47c170851df73a4b20c0469669e53338d89e4d
ksimpac <adasl0376210456789@gmail.com>
2022-07-01 09:47:48 +0800

    set a relationship with event

--- app/Collection.php
+++ app/Collection.php
@@ -9,4 +9,9 @@ class Collection extends Model
     protected $fillable = ['event_id', 'STU_ID'];
     protected $primaryKey = ['event_id', 'STU_ID'];
     public $incrementing = false;
+
+    public function event()
+    {
+        return $this->belongsTo(Event::class, 'event_id');
+    }
 }
